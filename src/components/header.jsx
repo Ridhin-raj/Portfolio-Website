1f40f9db647a7134333450c7dc2d859968a80abd
Ridhin-raj <ridhinrjkm@gmail.com>
2025-09-07 09:15:35 +0530

    contact me button on the right top

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,12 @@
 import { motion } from "framer-motion";
-import { FiGithub, FiX, FiLinkedin } from "react-icons/fi";
-
-
+import { useState } from "react";
+import { FiGithub, FiLinkedin, FiMenu, FiX } from "react-icons/fi";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Header = () => {
+    // Toggle the Menu open/close
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen( !isOpen)
   return (
     
     <header className="absolute w-full  z-50  transition-all  duration-300">
@@ -68,7 +71,7 @@ const Header = () => {
                     animate={{ opacity:1, scale: 1}}
                     transition={{ delay: 1.3, duration: 0.8}}
                     className="text-gray-700 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-color duration-300" href="#">
-                        <FiX className="w-5 h-5"/>
+                        <FaXTwitter className="w-5 h-5"/>
                     </motion.a>
                     <motion.a 
                     initial={{ opacity: 0, scale: 0.5}}
@@ -79,7 +82,22 @@ const Header = () => {
                     </motion.a>
                 </div>
 
-                
+                <div>
+                    {/* contact button  */}
+                     <motion.button 
+                     initial={{ opacity: 0, scale: 0.5}}
+                     animate={{ opacity: 1, scale: 1}}
+                     transition={{
+                        delay: 1.6,
+                        duration: 0.8,
+                        type: "spring",
+                        stiffness: 100,
+                        damping: 15,
+                     }}
+                     className="ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700  hover:text-white transition-all duration-500">
+                        Contact Me
+                     </motion.button>
+                </div>
 
             </div>
 
