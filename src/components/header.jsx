0f4e8d45a8b1b79ebba01f9c524709296cf902d4
Ridhin-raj <ridhinrjkm@gmail.com>
2025-09-07 14:49:14 +0530

    contact from for desktop

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { FiGithub, FiLinkedin, FiMenu, FiX } from "react-icons/fi";
 import { FaXTwitter } from "react-icons/fa6";
@@ -7,6 +7,11 @@ const Header = () => {
     // Toggle the Menu open/close
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = () => setIsOpen( !isOpen)
+
+    //  State to track if the contact form is  open 
+    const [contactFormOpen, setContactFormOpen ] = useState(false);
+    const openContactForm = () => setContactFormOpen(true)
+    const closeContactForm = () => setContactFormOpen(false)
   return (
     
     <header className="absolute w-full  z-50  transition-all  duration-300">
@@ -63,28 +68,31 @@ const Header = () => {
                     initial={{ opacity: 0, scale: 0.5}}
                     animate={{ opacity:1, scale: 1}}
                     transition={{ delay: 1.3, duration: 0.8}}
-                    className="text-gray-500 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-colors duration-300" href="#">
+                    className="text-gray-500 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-300" href="#">
                         <FiGithub className="w-5 h-5"/>
                     </motion.a>
                     <motion.a 
                     initial={{ opacity: 0, scale: 0.5}}
                     animate={{ opacity:1, scale: 1}}
                     transition={{ delay: 1.3, duration: 0.8}}
-                    className="text-gray-500 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-color duration-300" href="#">
+                    className="text-gray-500 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transition-colors
+ duration-300" href="#">
                         <FaXTwitter className="w-5 h-5"/>
                     </motion.a>
                     <motion.a 
                     initial={{ opacity: 0, scale: 0.5}}
                     animate={{ opacity:1, scale: 1}}
                     transition={{ delay: 1.3, duration: 0.8}}
-                    className="text-gray-500 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-color duration-300" href="#">
+                    className="text-gray-500 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transition-colors
+ duration-300" href="#">
                         <FiLinkedin className="w-5 h-5"/>
                     </motion.a>
                 </div>
 
                 <div>
                     {/* contact button  */}
-                     <motion.button 
+                     <motion.button
+                     onClick={openContactForm} 
                      initial={{ opacity: 0, scale: 0.5}}
                      animate={{ opacity: 1, scale: 1}}
                      transition={{
@@ -141,12 +149,80 @@ const Header = () => {
                    
                 </div>
                  <button 
-                    onClick={toggleMenu}
+                    onClick={() => {
+                        toggleMenu()
+                        
+                    }
+                    }
                     className="mt-4 block w-full px-4 py-2 rounded-lg bg-gradient-to-r from-violet-600 to-violet-400 font-bold">Contact Me</button>
+
+                    
 
             </div>
 
         </motion.div>
+        {/* contact form */}
+        <AnimatePresence>
+        {contactFormOpen && (
+            <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1}}
+            exit={{ opacity: 0}}
+            transition={{ duration: 0.5 }}
+            className="fixed inset-0 bg-black/50 backdrop-blur-sm z-50 flex items-center justify-center p-4" 
+            >
+                <motion.div 
+                initial={{ scale: 0.8 , opacity: 0 ,y: 30}}
+                animate={{ scale: 1, opacity: 1, y: 0}}
+                exit={{ scale: 0.8 , opacity: 0 ,y: 30}}
+                transition={{
+                    type: "spring",
+                    damping: 30,
+                    stiffness: 200,
+                    duration: 0.8,
+
+                }}
+                className="bg-white dark:bg-gray-800 rounded-xl shadow-xl w-full max-w-md p-6">
+                    <div className="flex justify-between items-center mb-4"> 
+                        <h1 className="text-2xl font-bold text-gray-300"> Get In Touch</h1>
+                        <button onClick={closeContactForm}>
+                            <FiX className="w-5 h-5 text-gray-300 font-extrabold"/>
+                        </button>
+
+                    </div>
+                    {/* Input Form */}
+
+                    <form className="space-4">
+
+                        <div>
+                            <label htmlFor="name" className="block text-sm font-medium text-gray-300 mb-1"> Name</label>
+                            <input type="text" id="name" placeholder="Your Name" className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700"/>
+                        </div>
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-300 mb-1"> Email</label>
+                            <input type="Email" id="Email" placeholder="Your Email" className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700"/>
+                        </div>
+                        <div>
+                            <label htmlFor="message" className="block text-sm font-medium text-gray-300 mb-1"> Message</label>
+                            <textarea rows="message"  id="message" placeholder="How can we help you?" className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700"/>
+                        </div>
+                        <motion.button 
+                        type="submit"
+                        whileHover={{scale: 1.03}}
+                        whileTap={{ scale: 0.97}}
+                        className="w-full px-4 py-2 bg-gradient-to-r from-violet-600 to-violet-400 hover:from-violet-700 hover:to-purple-700 transition-all duration-300 rounded-lg shadow-md hover:shadow-lg hover:shadow-violet-600">
+                            Send Message
+                        </motion.button>
+                    </form>
+
+                </motion.div>
+
+            </motion.div>
+        )
+        }
+        </AnimatePresence>
+
+
 
     </header>
   )
