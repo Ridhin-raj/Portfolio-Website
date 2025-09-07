fca5530623e7846fd70fac14f1b46cdf8195f0e8
Ridhin-raj <ridhinrjkm@gmail.com>
2025-09-07 16:19:28 +0530

    robo section text

---
+++ src/components/Robosectiontext.jsx
@@ -0,0 +1,20 @@
+
+
+const Robosectiontext = () => {
+  return (
+    <div className='z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text'>
+        {/* main text for desktop view */}
+        <div className=' flex-col hidden md:flex c-space'>
+            <h1 className="text-4xl font-medium ">
+            Hai i'm Ridhin</h1>
+            <div className="flex flex-col items-start">
+               <p className="text-5xl font-medium text-neutral-300">A Developer Dedicated to Crafting </p>
+               {/* <div>get flip word from aceternity</div> */}
+               <p className="text-neutral-300 ">Smart Solutions</p>
+            </div>
+            </div>
+    </div>
+  )
+}
+
+export default Robosectiontext
--- src/components/robosection.jsx
+++ src/components/robosection.jsx
@@ -1,10 +1,13 @@
 
-
+import Robosectiontext from "./Robosectiontext"
 const Robosection = () => {
   return (
     <section className="h-screen bg-gradient-to-b from-violet-900 to-black 
     flex xl:flex-row flex-col-reverse items-center justify-between lg:px-24
      px-10 relative overflow-hidden ">
+
+        {/* left text section */}
+        <Robosectiontext/>
        
      </section>
   )
