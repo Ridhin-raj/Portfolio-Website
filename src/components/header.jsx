1301c459f54ca3fb59fc482d48192c357d8a964b
Ridhin-raj <ridhinrjkm@gmail.com>
2025-09-07 12:25:32 +0530

    Mobile Menu Button & Mobile menu for header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     
     <header className="absolute w-full  z-50  transition-all  duration-300">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+            
                 {/* Logo/Name */}
                 <motion.div 
                 initial={{opacity: 0,x: -100}}
@@ -48,7 +48,7 @@ const Header = () => {
                                 damping: 20,
                                 delay: 0.7 + index * 0.2,
                             }}
-                            className="relative text-gray-700 dark:text-gray-200 hover:text-violet-600 dark:hover:text-violet-400 font-medium transition-colors duration-300 group"
+                            className="relative text-gray-500 dark:text-gray-200 hover:text-violet-600 dark:hover:text-violet-400 font-medium transition-colors duration-300 group"
                             href="#">
                                 {item}
                                 <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-violet-600 group-hover:w-full transition-all duration-300"></span>
@@ -63,21 +63,21 @@ const Header = () => {
                     initial={{ opacity: 0, scale: 0.5}}
                     animate={{ opacity:1, scale: 1}}
                     transition={{ delay: 1.3, duration: 0.8}}
-                    className="text-gray-700 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-color duration-300" href="#">
+                    className="text-gray-500 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-colors duration-300" href="#">
                         <FiGithub className="w-5 h-5"/>
                     </motion.a>
                     <motion.a 
                     initial={{ opacity: 0, scale: 0.5}}
                     animate={{ opacity:1, scale: 1}}
                     transition={{ delay: 1.3, duration: 0.8}}
-                    className="text-gray-700 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-color duration-300" href="#">
+                    className="text-gray-500 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-color duration-300" href="#">
                         <FaXTwitter className="w-5 h-5"/>
                     </motion.a>
                     <motion.a 
                     initial={{ opacity: 0, scale: 0.5}}
                     animate={{ opacity:1, scale: 1}}
                     transition={{ delay: 1.3, duration: 0.8}}
-                    className="text-gray-700 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-color duration-300" href="#">
+                    className="text-gray-500 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transiition-color duration-300" href="#">
                         <FiLinkedin className="w-5 h-5"/>
                     </motion.a>
                 </div>
@@ -94,14 +94,59 @@ const Header = () => {
                         stiffness: 100,
                         damping: 15,
                      }}
-                     className="ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700  hover:text-white transition-all duration-500">
+                     className=" md:flex hidden ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700  hover:text-white transition-all duration-500">
                         Contact Me
                      </motion.button>
                 </div>
 
+                
+
+                {/*  Mobile Menu Button */}
+                <div className="md:hidden flex items-center">
+                    <motion.button 
+                    whileTap={{scale: 0.7}}
+                    onClick={toggleMenu}
+                    className="text-gray-300">
+                        { isOpen ? <FiX className="h-6 w-6"/> : <FiMenu className="h-6 w-6"/> }
+                    </motion.button>
+
+                </div>
+        </div>
+        {/* Mobile menu */}
+        <motion.div 
+        initial={{ opacity: 0, height: 0}}
+        animate={{
+            opacity: isOpen ? 1 : 0,
+            height: isOpen ? "auto" : 0,
+        }}
+        transition={{duration: 0.5}}        className="md:hidden overflow-hidden bg-gray-600 dark:bg-gray-900 shadow-lg px-4 py-5 space-y-5">
+            <nav className="flex flex-col space-y-3">
+                {["Home", "About", "Projects", "Experience", "Contact"].map((item) => (
+                    <a onClick={toggleMenu} className="text-gray-300 font-medium py-2" key={item} href="#">{item}</a>
+                ))}
+
+            </nav>
+
+            <div className="pt-4 border-t border-gray-200 dark:border-gray-700"> 
+                <div className="flex space-x-5">
+                    <a href="#">
+                        <FiGithub className="h-5 w-5 text-gray-300"/>
+                    </a>
+                    <a href="#">
+                        <FiLinkedin className="h-5 w-5 text-gray-300"/>
+                    </a>
+                    <a href="#">
+                        <FaXTwitter className="h-5 w-5 text-gray-300"/>
+                    </a>
+                   
+                </div>
+                 <button 
+                    onClick={toggleMenu}
+                    className="mt-4 block w-full px-4 py-2 rounded-lg bg-gradient-to-r from-violet-600 to-violet-400 font-bold">Contact Me</button>
+
             </div>
 
-        </div>
+        </motion.div>
 
     </header>
   )
